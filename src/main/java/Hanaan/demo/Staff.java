50c927a8ee67c35f31d449519d993cfe6e6faf78
xanaa-ayan <ruweydabashir22@gmail.com>
2025-05-04 19:12:29 +0300

    Add files via upload

---
+++ src/main/java/Hanaan/demo/Staff.java
@@ -0,0 +1,40 @@
+package Hanaan.demo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Staff {
+    private  Long id;
+    private  String name;
+    private  String position;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
+
